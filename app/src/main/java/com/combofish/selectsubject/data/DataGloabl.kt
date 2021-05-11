cf2eae7c880e8a72fecead5a3a59cf1110621ff6
combofish <combofish@139.com>
2021-05-11 17:20:59 +0800

    Make url, account info to global data.

--- app/src/main/java/com/combofish/selectsubject/data/DataGloabl.kt
+++ app/src/main/java/com/combofish/selectsubject/data/DataGloabl.kt
@@ -1,4 +1,25 @@
 package com.combofish.selectsubject.data
 
-data class DataGlobal(val url:String = "http://192.168.122.1:8080/SelectSubject2_war_exploded/")
+import com.combofish.selectsubject.bean.Course
+import com.select.bean.Account
 
+
+object DataGlobal{
+    var url: String = "http://192.168.131.224:8080/SelectSubject2_war_exploded/"
+    val url1 :String = "http://192.168.131.224:8080/SelectSubject2_war_exploded/"
+    val url2 :String = "http://192.168.122.1:8080/SelectSubject2_war_exploded/"
+
+    var accountId: Int = 0
+
+    // 不在使用
+    // var accountStatus: Int = 0
+    // var accountStatusStr:String = ""
+
+    // 改用用户存储用户数据
+    var account = Account()
+
+    // main fragment id
+    var mainFragmentId:Int = 0
+
+}
+//http://192.168.122.1:8080/SelectSubject2_war_exploded/
